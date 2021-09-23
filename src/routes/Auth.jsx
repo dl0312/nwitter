16b18419df192391178480e3a205afe5ffa20302
geon lee <geon.lee@navercorp.com>
2021-09-23 13:16:21 +0900

    2.1 Login Form part One

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,42 @@
-import React from "react"
+import React, { useState } from 'react'
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleOnChange = (e) => {
+    const {
+      target: { name, value },
+    } = e
+
+    console.log(name)
+
+    if (name === 'email') {
+      setEmail(value)
+    } else if (name === 'password') {
+      setPassword(value)
+    }
+  }
+  return (
+    <div>
+      <form>
+        <input name="email" type="text" placeholder="Email" required value={email} onChange={handleOnChange} />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={handleOnChange}
+        />
+        <input type="sumbit" valeu="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  )
+}
 
 export default Auth

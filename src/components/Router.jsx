e6aad40e416aa928fdb4ebb29b9a59e621dad34b
geon lee <geon.lee@navercorp.com>
2021-09-23 17:02:15 +0900

    2.6 Log Out

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import Auth from '../routes/Auth'
-import Home from '../routes/Home'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import Profile from 'routes/Profile'
+import Auth from 'routes/Auth'
+import Home from 'routes/Home'
+import Navigation from 'components/Navigation'
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
           <>
             <Route exact path="/">
               <Auth />
             </Route>
+            <Redirect from="*" to="/" />
           </>
         )}
       </Switch>
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,4 +1,19 @@
-import React from "react"
+import { auth } from 'fbase'
+import React from 'react'
+import { useHistory } from 'react-router'
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+  const history = useHistory()
+
+  const handleOnLogOutClick = () => {
+    auth.signOut()
+    history.push('/')
+  }
+
+  return (
+    <>
+      <button onClick={handleOnLogOutClick}>Log Out</button>
+    </>
+  )
+}
 export default Profile

e2cf2c0fccf3d247ef18682a4818765e5cdf9af9
geon lee <geon.lee@navercorp.com>
2021-09-23 19:33:31 +0900

    3.0 Form and Database Setup

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useState } from 'react'
 
-const Home =  () => {
-    return(
-        <span>Home</span>
-    )
+const Home = () => {
+  const [nweet, setNweet] = useState('')
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    console.log(nweet)
+  }
+
+  const handleOnChange = (e) => {
+    const {
+      target: { value },
+    } = e
+    setNweet(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" placeholder="What's on your mind?" maxLength={120} onChange={handleOnChange} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  )
 }
 export default Home
